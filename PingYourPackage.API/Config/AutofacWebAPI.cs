38b9bd524124cd88df66ebb7b7c21ffd667d1fdf
zhibeiyou <xuweiluwx@gmail.com>
2016-04-07 18:14:23 +0800

    PingYourPackage Initialized

---
+++ PingYourPackage.API/Config/AutofacWebAPI.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+public class AutofacWebAPI
+{
+    public static void Initialize(HttpConfiguration config)
+    {
+        Initialize(config, RegisterServices(new ContainerBuilder()));
+    }
+    public static void Initialize(HttpConfiguration config, IContainer container)
+    {
+        config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+    }
+    private static IContainer RegisterServices(ContainerBuilder builder)
+    {
+        builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+        //registeration goes here
+
+        return builder.Build();
+    }
+}
+//namespace PingYourPackage.API.Config
+//{
+
+//}
